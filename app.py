c35f6ba74baec6009183c9ca8de38013c265cf47
Javid Abbasly <94132865+Javid100@users.noreply.github.com>
2023-09-22 19:41:41 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+
+
+#Tapsiriq no1
+num1=int(input("Enter Num1"))
+num2=int(input("Enter Num2"))
+print(num1+num2)
+
+num1=int(input("Enter Num1"))
+num2=int(input("Enter Num2")) 
+print(num1*num2)
+
+num1=int(input("Enter Num1"))
+num2=int(input("Enter Num2")) 
+print(num1-num2)
+
+num1=int(input("Enter Num1"))
+num2=int(input("Enter Num2")) 
+print(num1/num2)
+
+
+
+
+#Tapsiriq no2
+name=str(input("enter your name"))
+surname=str(input("enter your surname"))
+age=int(input("enter your age"))
+print(f"Menim adim {name} soyadim {surname} yasim {age}")
+
+
+
+
+
